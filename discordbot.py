71fc64f5f136588d674447ca20854811fcca1913
takaoka <takaoka@mokosoft.com>
2021-04-06 13:39:10 +0900

    nihongo ikeru?

--- discordbot.py
+++ discordbot.py
@@ -18,6 +18,13 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('わいや')
 
+
+@bot.command()
+async def ねむい(ctx):
+    rand_int = random.randint(0,100)
+    await ctx.send(f"{ctx.author.mention}"+' 眠気は '+str(rand_int)+' やで' )
+
+
 @bot.command()
 async def roll(ctx):
     rand_int = random.randint(0,100)
@@ -26,7 +33,7 @@ async def roll(ctx):
 
 @bot.command()
 async def massa(ctx):
-    await ctx.send('<:Massa:> <:uruse:>')
+    await ctx.send('<:Massa:761401088540672010> <:uruse:760475866626785342>')
 
 
 
