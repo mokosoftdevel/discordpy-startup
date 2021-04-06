2b54d22dd2db1a8c3beae29eb77adec6e2f4bd48
takaoka <takaoka@mokosoft.com>
2021-04-06 13:15:37 +0900

    rand

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +17,12 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('わいや')
+
+@bot.command()
+async def roll(ctx):
+    rand_int = random.randint(0,100)
+    await ctx.send('うんこのかんじは '+rand_int+' やな')
+
 
 
 bot.run(token)
