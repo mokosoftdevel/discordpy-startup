cb60ce4f16d1d8c8f5ee9657afcb059e00360d9d
takaoka <takaoka@mokosoft.com>
2021-04-23 12:41:36 +0900

    asdfaf

--- discordbot.py
+++ discordbot.py
@@ -215,8 +215,10 @@ async def func_get_unko_message_spreadsheet():
     global unko_messages
     unko_messages.clear()
     last_line = int(sheet_messages.cell(1,2).text)
+    print(last_line)
     ranges = sheet_messages.range(3,1,last_line,5)
     print(ranges)
+
     
 
 
