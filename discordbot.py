16399296967f342304d6805ea77d5d4f642eac0b
takaoka <takaoka@mokosoft.com>
2021-06-23 19:51:27 +0900

    asdf

--- discordbot.py
+++ discordbot.py
@@ -182,7 +182,7 @@ async def com_slot7(ctx):
     for x in range(1,8):
         for y in range(1,8):
             for z in range(1,8):
-                dice.append(100*x+10*y*z)
+                dice.append(100*x+10*y+z)
     print(dice)
 
     prob = list()
