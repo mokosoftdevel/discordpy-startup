36571dc3626101721ced6912cb9611419bc40304
mokosoft <takaoka@mokosoft.com>
2022-06-09 14:02:14 +0900

    f

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from discord.ext import commands
+import discord
+import os
+
+bot = commands.Bot()
+token = os.environ['DISCORD_BOT_TOKEN']
+bot.run(token)
 
 
 # web 
 app = Flask(__name__)
 @app.route('/')
-def web_main():
+async def web_main():
+    channel = bot.get_channel(700201258929094778)
+    await channel.send("uhihihi")
     return "hello world"
 
 if __name__ == "__main__":
