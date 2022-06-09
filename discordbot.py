1850924141a7b6704ccaadf2a3dd571bba83ef37
mokosoft <takaoka@mokosoft.com>
2022-06-09 12:22:39 +0900

    f

--- discordbot.py
+++ discordbot.py
@@ -661,10 +661,11 @@ bot.run(token)
 
 
 # web 
+print("debug")
 print(__name__)
 app = Flask(__name__)
 @app.route('/')
 def web_main():
     return "hello world"
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="8080")
+    app.run(debug=True)
